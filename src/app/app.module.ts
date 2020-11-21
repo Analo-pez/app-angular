aa8671b3c082077e71448befb77ec138f1866116
Analo-pez <ana.ts.lopez@gmail.com>
2020-11-21 17:28:15 +0100

    version v0.9 beta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+
+// Routes
 import { AppRoutingModule } from './app-routing.module';
+
+
+//Services
+
+
+
+// Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormComponent } from './components/form/form.component';
 import { ResultComponent } from './components/result/result.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 
 
 @Component({
@@ -7,35 +6,41 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './form.component.html',
     styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
-
-    ngOnInit() { }
-
-    searchResult(repo: string) {
-        console.log(repo)
-    }
+export class FormComponent {
 
     showForm = true;
 
     getIssuesGithub = () => {
-        console.log('han clickado')
-        // const name = document.querySelector(".js-name");
-        // const repo = document.querySelector(".js-repo");
-        // const username = name.value;
-        // const reponame = repo.value;
+        const name = document.querySelector(".js-name");
+        const repo = document.querySelector(".js-repo");
+        const results2 = document.querySelector('.js-result2');
+        const username = name.value;
+        const reponame = repo.value;
 
-        // fetch(`https://api.github.com/repos/${username}/${reponame}/issues`)
-        //     .then(response => response.json())
-        //     .then(data => {
-        //         console.log(data)
-        //         data.map(issues => {
-        //             const results2 = document.querySelector('.js-result2');
-        //             return results2.innerHTML += `<ul>
-        //             <li> Title: ${issues.title} </li>
-        //             <li> State: ${issues.state}  </li>
-        //             <li> Create at: ${issues.created_at}</li></ul>`
-        //         })
-        //     });
+        fetch(`https://api.github.com/repos/${username}/${reponame}/issues`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                if (data == "") {
+                    return results2.innerHTML += `<div class="card"><p> No hay issues en este repositorio</p></div>`
+
+                } else {
+                    data.map(issues => {
+                        return results2.innerHTML += `
+                        <div class="card" style="width: 18rem;">
+                        <img src=${issues.user.avatar_url}  class="card-img-top" alt="Author-image">
+                        <div class="card-body">
+                          <h5 class="card-title">Title: ${issues.title}</h5>
+                          <p class="card-text">Author: ${issues.user.login}</p>
+                          <p class="card-text">State: ${issues.state}</p>
+                          <p class="card-text">Description: ${issues.body}</p>
+                          <p class="card-text">Created at: ${issues.created_at}</p>
+                          <a class="btn btn-primary" href=${issues.html_url} target="_blank">Link to issue </a>
+                        </div>
+                      </div>`
+                    })
+                }
+            });
     }
 
 }
